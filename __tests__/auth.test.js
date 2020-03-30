fa7be400af2ea16d9c1635cf9da42ee71de2c205
Will G Piro <willgpiro@gmail.com>
2020-03-30 16:40:58 -0700

    authorize method, login test, login route checks U/P creates token

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -4,8 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
 
-describe('app routes', () => {
+describe('auth routes', () => {
   beforeAll(() => {
     connect();
   });
@@ -16,5 +17,32 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+  //signs up a user will not include password or password hash as it will have been scrubbed off.
+  it('signs up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'will', password: 'willWasHere' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'will',
+          __v: 0
+        });
+      });
+  });
+  //we cant actual test loging in a user unless we have a user... bring in from model. 
+  it('logs in a user', async() => {
+    await User.create({ username: 'will', password: 'willWasHere' });
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ username: 'will', password: 'willWasHere' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'will',
+          __v: 0
+        });
+      });
   });
 });
