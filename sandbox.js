d949bc908b7c52dcafed23317bfd434891a2dd12
Will G Piro <willgpiro@gmail.com>
2020-03-30 13:52:30 -0700

    added added instance method authToken to model and tested

---
+++ sandbox.js
@@ -0,0 +1,10 @@
+const User = require('./lib/models/User');
+
+//we can checkout how our virutual password works by creating a user and running program . we have a password hash
+const user = new User({
+  username: 'spot',
+  password: 'spotWasHere'
+});
+//the console log version of this password is only the has version not the actual password. 
+console.log(user.toJSON());
+
