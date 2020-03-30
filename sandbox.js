fee63aac130bf303977a39abf570d7509a9f4381
Will G Piro <willgpiro@gmail.com>
2020-03-30 14:07:48 -0700

    toJSON deletes password hash

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,5 @@
 const User = require('./lib/models/User');
-
+require('dotenv').config();
 //we can checkout how our virutual password works by creating a user and running program . we have a password hash
 const user = new User({
   username: 'spot',
@@ -7,4 +7,6 @@ const user = new User({
 });
 //the console log version of this password is only the has version not the actual password. 
 console.log(user.toJSON());
+
+console.log(user.authToken());
 
